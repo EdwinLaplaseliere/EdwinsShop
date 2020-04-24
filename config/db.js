b0f119a13e62f678de598930b4e40ab30b9ccddd
EdwinLaplaseliere <edwingarces8@gmail.com>
2020-04-24 17:08:43 +0000

    stuck

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/EmployeeDB?retryWrites=true&w=majority',
-{userNewUrlParser:true},(err)=>{
+{useNewUrlParser:true},(err)=>{
 if(!err){console.log('Yesss')}
 else{console.log('nooo')}
 
