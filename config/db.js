48c49cb17c7fca590e904c7af0a43b2b6f8377bb
EdwinLaplaseliere <edwingarces8@gmail.com>
2020-04-24 17:05:56 +0000

    stuck

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
-module.exports= {
-url: "mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/EmployeeDB?retryWrites=true&w=majority"
-}
+const mongoose=require('mongoose');
+mongoose.connect('mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/EmployeeDB?retryWrites=true&w=majority',
+{userNewUrlParser:true},(err)=>{
+if(!err){console.log('Yesss')}
+else{console.log('nooo')}
+
+});
+
+// module.exports= {
+// url: "mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/EmployeeDB?retryWrites=true&w=majority"
+// }
 
 require('./employee.model');
