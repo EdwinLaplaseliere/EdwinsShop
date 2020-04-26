4b6cad980982298bd9226e101ddde2c91d440ef5
EdwinLaplaseliere <edwingarces8@gmail.com>
2020-04-26 22:24:32 +0000

    last change

--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 /**
  * This is the link to connect to the data base with the username and pasword
  */
-mongoose.connect('mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/ItemDB?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('You are connected') }
     else { console.log('Something happend, please try again : ' + err) }
 });
