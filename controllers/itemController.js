66dde7e892153eb80acacca15aa50dbdb8e2f0fd
EdwinLaplaseliere <edwingarces8@gmail.com>
2020-04-26 22:16:57 +0000

    last change

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose'); // we need mongoose for the database
 const Item = mongoose.model('Item');// item is the database collection, it makes an Item object and sends it to the database
 const Handlebars = require('handlebars')// Handlebars is used to implement java scripts in the templates 
 
-require('dotenv').config();
+
 
 
 /**
