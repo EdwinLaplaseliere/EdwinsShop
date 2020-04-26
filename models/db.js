acf0e942cdced20d83e9d7618c95943b40cb0cb1
EdwinLaplaseliere <edwingarces8@gmail.com>
2020-04-26 22:34:47 +0000

    My app

--- models/db.js
+++ models/db.js
@@ -3,11 +3,11 @@
  */
 const mongoose = require('mongoose');
 
-
+require('dotenv').config();
 /**
  * This is the link to connect to the data base with the username and pasword
  */
-mongoose.connect('mongodb+srv://EdGarces:pass1234!@cluster0-54wkk.mongodb.net/ItemDB?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('You are connected') }
     else { console.log('Something happend, please try again : ' + err) }
 });
